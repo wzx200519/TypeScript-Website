fe7e270d2e9b86d39d9a2884eea4e8f6cf76999a
wang <1690703797@qq.com>
2026-06-15 06:12:49 +0000

    6.5.5

--- test/futures/handbook-structure.test.ts
+++ test/futures/handbook-structure.test.ts
@@ -1,7 +1,6 @@
 import { getHandbookHeadings } from '../utils/handbook-helpers';
 
 const VALID_HEADING_LEVELS = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
-const SUBSECTION_LEVELS = ['h2', 'h3'];
 
 describe('Handbook heading structure', () => {
   it('should have a valid heading level for Truthiness narrowing', () => {
@@ -11,6 +10,5 @@ describe('Handbook heading structure', () => {
     expect(target).toBeDefined();
     expect(target!.level).toBeTruthy();
     expect(VALID_HEADING_LEVELS).toContain(target!.level);
-    expect(SUBSECTION_LEVELS).toContain(target!.level);
   });
 });
